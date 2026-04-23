c049fe432ff98cfca1f996007f2c408a182da0dd
Nourhan Adel <91224846+NorhanAdel@users.noreply.github.com>
2026-04-23 19:02:42 +0200

    Delete app directory

--- app/graphql/mutation/favorite.mutations.ts
+++
@@ -1,5 +0,0 @@
-export const TOGGLE_FAVORITE = `
-  mutation ToggleFavorite($input: AddFavoriteInput!) {
-    toggleFavorite(input: $input)
-  }
-`;
--- app/graphql/query/languages.query.tsx
+++
@@ -1,9 +0,0 @@
-export const GET_ACTIVE_LANGUAGES = `
-  query GetActiveLanguages {
-    activeLanguages {
-      id
-      code
-      name
-    }
-  }
-`;
